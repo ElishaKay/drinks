09a3e2ed0421bf5f2fd77a058c6778a0e353fc56
elishakay <Kramer1346@gmail.com>
2025-02-20 12:18:40 +0200

    setup

---
+++ src/pages/AddDrinkPage.tsx
@@ -0,0 +1,12 @@
+import AddNewDrink from "../components/AddNewDrink";
+
+const AddDrinkPage = () => {
+    return (
+        <div>
+            <h1>Add a New Drink</h1>
+            <AddNewDrink />
+        </div>
+    );
+};
+
+export default AddDrinkPage;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import CocktailList from "../components/CocktailList";
+import SearchBar from "../components/SearchBar";
+
+const Home = () => {
+    return (
+        <div>
+            <SearchBar />
+            <CocktailList />
+        </div>
+    );
+};
+
+export default Home;
