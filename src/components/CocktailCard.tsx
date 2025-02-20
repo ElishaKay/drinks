6217a1cf0c9eeed33364f13678404dc7a8a77597
elishakay <Kramer1346@gmail.com>
2025-02-20 13:55:06 +0200

    polish styling

--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -6,9 +6,34 @@ interface CocktailCardProps {
 
 const CocktailCard: React.FC<CocktailCardProps> = ({ cocktail }) => {
     return (
-        <Link to={`/cocktail/${cocktail.idDrink}`} className="cocktail-card">
-            <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
-            <h3>{cocktail.strDrink}</h3>
+        <Link 
+            to={`/cocktail/${cocktail.idDrink}`} 
+            className="cocktail-card"
+            style={{
+                maxWidth: "600px",
+                maxHeight: "600px", 
+                display: "block",
+                overflow: "hidden"
+            }}
+        >
+            <img 
+                src={cocktail.strDrinkThumb} 
+                alt={cocktail.strDrink}
+                style={{
+                    width: "100%",
+                    height: "400px",
+                    objectFit: "cover"
+                }}
+            />
+            <h3 style={{
+                margin: "20px 0",
+                fontSize: "2rem",
+                textOverflow: "ellipsis", 
+                overflow: "hidden",
+                whiteSpace: "nowrap"
+            }}>
+                {cocktail.strDrink}
+            </h3>
         </Link>
     );
 };
